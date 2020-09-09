519cdef00628eabb04c73d51aae6fec3a92cdaab
Md. Nuruddin Adnan <nuruddin.adnan1993@gmail.com>
2020-09-09 15:30:56 +0600

    two page complete

---
+++ js/custom.js
@@ -0,0 +1,12 @@
+$(window, document, undefined).ready(function() {
+
+  $('.contact input').blur(function() {
+    var $this = $(this);
+    if ($this.val())
+      $this.addClass('used');
+    else
+      $this.removeClass('used');
+  });
+
+
+});
